217de4f9d98aacc095bbe42757ccf71f0460fdd0
kimchy123 <504124343@qq.com>
2017-05-23 15:45:24 +0800

    delete

--- TradeFactory.java
+++
@@ -1,8 +0,0 @@
-package xiyue.simtrade.traderapi;
-
-/**
- * Created by chenyan on 2017/5/17.
- */
-public abstract class TradeFactory {
-    public abstract TradeService getInstance(String userId, String sessionId);
-}
